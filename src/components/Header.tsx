4533298b92d9d5c8060c960c5e92301641ef4035
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-06-07 15:46:17 +0400

    bottom design of the header is done for now

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import icon from '../assets/logo.png'
 import Image from 'next/image'
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { IoIosSearch } from 'react-icons/io'
 
 const links = [
   { icon: MdOutlineMailOutline, href: '/inbox' },
@@ -14,53 +15,69 @@ const links = [
 
 const Header = () => {
   return (
-    <header className='w-full flex items-center justify-between'>
-      <Link href='/' className='relative w-[200px] -ml-[25px] h-[100px]'>
-        <Image
-          src={icon}
-          alt='store-icon-image'
-          className='object-contain'
-          fill
-        />
-      </Link>
-      <nav className='flex items-center gap-8'>
-        <Link
-          href='/sell-item'
-          className='bg-[#FFF4CC] flex hover:bg-[#FFEFB2] cursor-pointer transition-all ease-linear items-center h-[42px] w-[130px] justify-center rounded-xl gap-2'
-        >
-          <FiPlusCircle className='text-mainYellow text-[20px]' />
-          <span className='text-sm'>დამატება</span>
+    <header className='flex flex-col items-start gap-9'>
+      <div className='w-full flex items-center justify-between relative'>
+        <div className='absolute w-screen h-[1px] bg-gray-200 bottom-1.5 left-1/2 translate-x-[-50%]' />
+        <Link href='/' className='relative w-[200px] -ml-[25px] h-[100px]'>
+          <Image
+            src={icon}
+            alt='store-icon-image'
+            className='object-contain'
+            fill
+          />
         </Link>
-        <div className='flex items-center gap-3'>
-          {links.map((link, idx) => {
-            return (
-              <Link
-                key={idx}
-                href={link.href}
-                className='p-2.5 hover:bg-[#F6F6F6] rounded-full transition-all ease-linear'
-              >
-                <link.icon
-                  className={`${
-                    link.href === '/inbox' ? 'text-[25px]' : 'text-[22.5px]'
-                  }`}
-                />
-              </Link>
-            )
-          })}
-        </div>
-        <SignedOut>
+        <nav className='flex items-center gap-8'>
           <Link
-            href='/login'
-            className='flex items-center cursor-pointer gap-2 hover:bg-[#F6F6F6] transition-all ease-linear w-[120px] h-[42px] justify-center border rounded-xl'
+            href='/sell-item'
+            className='bg-[#FFF4CC] flex hover:bg-[#FFEFB2] cursor-pointer transition-all ease-linear items-center h-[42px] w-[130px] justify-center rounded-xl gap-2'
           >
-            <FiUser className='text-xl' />
-            <span>შესვლა</span>
+            <FiPlusCircle className='text-mainYellow text-[20px]' />
+            <span className='text-sm'>დამატება</span>
           </Link>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </nav>
+          <div className='flex items-center gap-3'>
+            {links.map((link, idx) => {
+              return (
+                <Link
+                  key={idx}
+                  href={link.href}
+                  className='p-2.5 hover:bg-[#F6F6F6] rounded-full transition-all ease-linear'
+                >
+                  <link.icon
+                    className={`${
+                      link.href === '/inbox' ? 'text-[25px]' : 'text-[22.5px]'
+                    }`}
+                  />
+                </Link>
+              )
+            })}
+          </div>
+          <SignedOut>
+            <Link
+              href='/login'
+              className='flex items-center cursor-pointer gap-2 hover:bg-[#F6F6F6] transition-all ease-linear w-[120px] h-[42px] justify-center border rounded-xl'
+            >
+              <FiUser className='text-xl' />
+              <span>შესვლა</span>
+            </Link>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </nav>
+      </div>
+      <div className='flex flex-col items-start gap-6 w-full'>
+        <h2 className='text-[27px] font-medium'>ყველაფერი, რასაც ეძებ</h2>
+        <div className='flex items-center gap-4 w-full'>
+          <input
+            className='border-[3px] flex-grow px-5 py-5 placeholder:text-lg outline-none border-mainYellow rounded-xl'
+            placeholder='მაგ: Iphone 14'
+          />
+          <button className='flex items-center bg-[#FEC900] hover:bg-mainYellow cursor-pointer py-5 rounded-xl px-12 gap-2 text-[18px]'>
+            <IoIosSearch />
+            <span>ძებნა</span>
+          </button>
+        </div>
+      </div>
     </header>
   )
 }
