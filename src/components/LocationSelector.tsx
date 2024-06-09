67d740ef6ff77bd73b5121de65ef8984e639f0e3
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-06-09 19:22:20 +0400

    if we cant find the searched city then we will tell the user we couldnt find one

--- src/components/LocationSelector.tsx
+++ src/components/LocationSelector.tsx
@@ -24,25 +24,31 @@ const LocationSelector = () => {
 
   return (
     <div
-      className={`absolute z-[100] flex flex-col w-full top-[60px] max-h-[300px] overflow-y-scroll border py-4 text-[15.5px] left-1/2 items-start transition-all duration-300 
+      className={`absolute z-[100] flex flex-col w-full top-[60px] max-h-[300px] ${
+        citiesCopy.length >= 6 && 'overflow-y-scroll'
+      } border py-4 text-[15.5px] left-1/2 items-start transition-all duration-300 
     ease-out -translate-x-1/2 bg-white opacity-0 pointer-events-none rounded-xl ${
       locationBoxOpen && 'opacity-100 pointer-events-auto'
     }`}
     >
-      {citiesCopy.map((city: string, idx: number) => {
-        return (
-          <div
-            onClick={() =>
-              dispatch(selectSellerLocation({ selectedLocation: city }))
-            }
-            className='w-full py-2 cursor-pointer hover:bg-gray-200 
+      {citiesCopy.length !== 0 ? (
+        citiesCopy.map((city: string, idx: number) => {
+          return (
+            <div
+              onClick={() =>
+                dispatch(selectSellerLocation({ selectedLocation: city }))
+              }
+              className='w-full py-2 cursor-pointer hover:bg-gray-200 
         hover:text-blue-400 transition-all ease-linear px-5'
-            key={idx}
-          >
-            {city}
-          </div>
-        )
-      })}
+              key={idx}
+            >
+              {city}
+            </div>
+          )
+        })
+      ) : (
+        <h4 className='mx-auto font-medium text-gray-400'>არ მოიძებნა</h4>
+      )}
     </div>
   )
 }
