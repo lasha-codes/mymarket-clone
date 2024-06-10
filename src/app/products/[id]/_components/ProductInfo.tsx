302bd2a79d6b5c9dbcc3a1aff9ba8a0e65fb69f1
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-06-10 22:17:41 +0400

    stil not done with Purchase component design this thing does take a while man

--- src/app/products/[id]/_components/ProductInfo.tsx
+++ src/app/products/[id]/_components/ProductInfo.tsx
@@ -1,4 +1,6 @@
 import { IoEyeOutline } from 'react-icons/io5'
+import { LuClock4 } from 'react-icons/lu'
+import { format } from 'date-fns'
 
 type ProductInfoProps = {
   id: string
@@ -25,11 +27,15 @@ const ProductInfo = ({
 }: ProductInfoProps) => {
   return (
     <div className='w-full flex flex-col gap-5'>
-      <div className='flex w-full items-center justify-between'>
+      <div className='flex w-full items-center  gap-6'>
         <h3 className='font-medium text-[14px]'>ID {id}</h3>
-        <div className='flex items-center gap-1 text-[14px]'>
-          <IoEyeOutline className='text-gray-400' />
+        <div className='flex items-center gap-2 text-[14px] font-medium'>
+          <IoEyeOutline className='text-gray-400 text-[15px]' />
           <span>{views} ნახვა</span>
+        </div>
+        <div className='flex items-center gap-2 text-[14px] font-medium'>
+          <LuClock4 className='text-gray-400' />
+          <span>{updatedAt && format(updatedAt, 'dd/MM/yyyy HH:mm')}</span>
         </div>
       </div>
     </div>
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import ProductSlider from './_components/ProductSlider'
 import ProductInfo from './_components/ProductInfo'
+import Purchase from './_components/Purchase'
 
 const ProductInspectPage = ({ params }: { params: { id: string } }) => {
   const dispatch = useDispatch()
@@ -21,24 +22,31 @@ const ProductInspectPage = ({ params }: { params: { id: string } }) => {
   }, [products])
 
   return (
-    <section className='w-full px-8 py-10 flex items-center'>
-      <ProductSlider
-        condition={productById?.condition as string}
-        images={productById?.images || []}
-        youtubeURL={
-          productById?.youtubeURL ? (productById.youtubeURL as string) : ''
-        }
-      />
-      <ProductInfo
-        productDescription={productById?.description as string}
-        productTitle={productById?.name as string}
-        id={productById?.id as string}
-        sellerLocation={productById?.location as string}
-        sellerName={productById?.contactName as string}
-        sellerPhone={productById?.contactNumber as number}
-        views={productById?.views as number}
-        updatedAt={productById?.updatedAt as Date}
-        productPrice={productById?.price as number}
+    <section className='w-full px-8 py-10 flex items-start justify-between'>
+      <div className='flex items-start justify-between w-full gap-6'>
+        <ProductSlider
+          condition={productById?.condition as string}
+          images={productById?.images || []}
+          youtubeURL={
+            productById?.youtubeURL ? (productById.youtubeURL as string) : ''
+          }
+        />
+        <ProductInfo
+          productDescription={productById?.description as string}
+          productTitle={productById?.name as string}
+          id={productById?.id as string}
+          sellerLocation={productById?.location as string}
+          sellerName={productById?.contactName as string}
+          sellerPhone={productById?.contactNumber as number}
+          views={productById?.views as number}
+          updatedAt={productById?.updatedAt as Date}
+          productPrice={productById?.price as number}
+        />
+      </div>
+      <Purchase
+        product={productById as Product}
+        priceOffers={productById?.priceDealType as string[]}
+        price={productById?.price as number}
       />
     </section>
   )
