e371f16ea3489c3175f3d17a47dedcfedd3af780
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-06-11 17:03:58 +0400

    displaying the purchase product info aswell

--- src/app/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,17 +1,42 @@
 'use client'
-import { PaymentElement, Elements, CardElement } from '@stripe/react-stripe-js'
+import { PaymentElement, Elements } from '@stripe/react-stripe-js'
 import { useStripe, useElements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 import { Product } from '@prisma/client'
+import Image from 'next/image'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
+import ReactPlayer from 'react-player'
+import { useEffect, useState } from 'react'
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
 )
 
-const CheckoutForm = ({ client_secret }: { client_secret: string }) => {
+const CheckoutForm = ({
+  client_secret,
+  purchaseProduct,
+}: {
+  client_secret: string
+  purchaseProduct: Product
+}) => {
+  const [isClient, setIsClient] = useState<boolean>(false)
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
   return (
-    <div className='w-full h-screen'>
-      <h1>Payment Page</h1>
+    <div className='w-full flex items-start justify-around py-10 px-20 max-xl:px-16 max-lg:px-14 max-md:px-10 gap-20'>
+      {isClient && (
+        <div className='w-full'>
+          <ProductToPurchase purchaseProduct={purchaseProduct} />
+        </div>
+      )}
       <Elements
         stripe={stripePromise}
         options={{
@@ -30,9 +55,42 @@ function Form() {
   const elements = useElements()
   const stripe = useStripe()
   return (
-    <form>
-      <CardElement />
+    <form className='w-full'>
       <PaymentElement />
     </form>
+  )
+}
+
+function ProductToPurchase({ purchaseProduct }: { purchaseProduct: Product }) {
+  const { images, youtubeURL } = purchaseProduct
+  return (
+    <Carousel className='w-[450px] h-[420px] max-lg:h-[380px] max-lg:w-[420px] max-md:w-[380px] max-md:h-[350px] overflow-hidden border rounded-2xl'>
+      <CarouselContent>
+        {images &&
+          images.map((URL, idx: number) => {
+            return (
+              <CarouselItem
+                key={idx}
+                className='w-full h-[420px] max-lg:h-[380px] max-md:h-[350px] relative'
+              >
+                <Image
+                  src={URL}
+                  key={idx}
+                  fill
+                  className='object-cover'
+                  alt={'product image' + '' + { idx }}
+                />
+              </CarouselItem>
+            )
+          })}
+        {youtubeURL && (
+          <CarouselItem className='w-full h-[420px] max-lg:h-[380px] max-md:h-[350px] flex justify-center'>
+            <ReactPlayer url={youtubeURL} height={420} />
+          </CarouselItem>
+        )}
+      </CarouselContent>
+      <CarouselNext className='absolute right-3 h-[45px] w-[45px] bg-gray-200 hover:bg-gray-300 transition-all' />
+      <CarouselPrevious className='absolute left-3 w-[45px] h-[45px] bg-gray-200 hover:bg-gray-200 transition-all' />
+    </Carousel>
   )
 }
--- src/app/products/[id]/purchase/page.tsx
+++ src/app/products/[id]/purchase/page.tsx
@@ -1,25 +1,32 @@
 import { Stripe } from 'stripe'
 import CheckoutForm from './_components/CheckoutForm'
 import { getProductByIdPurchase } from '@/app/(auth)/utils/product'
+import { Product } from '@prisma/client'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 const StripePurchasePage = async ({ params }: { params: { id: string } }) => {
   const productId = params.id
-  console.log(productId)
-  console.log(productId)
-  const purchaseProduct = await getProductByIdPurchase(productId)
-  console.log(purchaseProduct)
+  const purchaseProduct: { product: Product } = await getProductByIdPurchase(
+    productId
+  )
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 1000,
-    currency: 'usd',
-    description: 'good product',
+    amount: purchaseProduct.product.price,
+    currency: purchaseProduct.product.bill === 'ლარი' ? 'gel' : 'usd',
+    description: purchaseProduct.product.description,
     metadata: {
-      title: 'Product',
+      product_image: purchaseProduct.product.images[0],
+      product_title: purchaseProduct.product.name,
+      product_price: purchaseProduct.product.price,
     },
   })
 
-  return <CheckoutForm client_secret={paymentIntent.client_secret as string} />
+  return (
+    <CheckoutForm
+      client_secret={paymentIntent.client_secret as string}
+      purchaseProduct={purchaseProduct.product}
+    />
+  )
 }
 
 export default StripePurchasePage
