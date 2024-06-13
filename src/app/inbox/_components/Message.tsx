0b90dc3e1e86bfca4ce0bfda7463602f7090e15c
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-06-13 17:17:16 +0400

    hide and show functionaility on the message

--- src/app/inbox/_components/Message.tsx
+++ src/app/inbox/_components/Message.tsx
@@ -33,12 +33,20 @@ const Message = ({
 
   const [showMessage, setShowMessage] = useState<boolean>(false)
 
+  const truncateMessage = (message: string) => {
+    if (message.length > 120) {
+      return `${message.slice(0, 120)}...`
+    } else {
+      return message
+    }
+  }
+
   const ReturnSentMessageComponent = () => {
     if (sent_message && sent_message.type === 'Purchase') {
       return (
-        <div className='bg-white shadow-md rounded-2xl px-5 py-3 flex flex-col gap-3 items-start max-w-[350px]'>
-          <div className='flex items-center gap-3'>
-            <div className='relative rounded-full overflow-hidden w-[30px] h-[30px]'>
+        <div className='bg-white shadow-md rounded-2xl transition-all duration-300 ease-linear px-5 py-3 flex flex-col gap-3 items-start max-w-[350px]'>
+          <div className='flex items-center gap-3 transition-all duration-300 ease-linear'>
+            <div className='relative rounded-full transition-all duration-300 ease-linear overflow-hidden w-[30px] h-[30px]'>
               <Image
                 src={sender?.photo as any}
                 fill
@@ -46,7 +54,7 @@ const Message = ({
                 alt='user profile'
               />
             </div>
-            <div className='flex items-center gap-3'>
+            <div className='flex items-center gap-3 transition-all duration-300 ease-linear'>
               <Link href='' className='text-[15px]'>
                 თქვენ
               </Link>
@@ -56,8 +64,19 @@ const Message = ({
               </Link>
             </div>
           </div>
-          <div>
-            <p className='text-sm text-slate-500'>{sent_message.message}</p>
+          <div className='flex flex-col items-end transition-all duration-300 ease-linear'>
+            <p className={`text-sm text-slate-500`}>
+              {showMessage
+                ? sent_message.message
+                : truncateMessage(sent_message.message)}
+            </p>
+
+            <button
+              onClick={() => setShowMessage(!showMessage)}
+              className='text-sm text-blue-600 font-medium'
+            >
+              {showMessage ? 'hide' : 'show'}
+            </button>
           </div>
         </div>
       )
