7ecff006137c36c679eb4d8e30ab9933d7f70838
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-06-14 13:20:00 +0400

    better design for the inbox and header has now optional rendering based on the path

--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -2,38 +2,45 @@
 import { useSelector } from 'react-redux'
 import Message from './_components/Message'
 import { Messages } from '@prisma/client'
+import Header from '@/components/Header'
 
 const InboxPage = () => {
   const { messages, users } = useSelector((state: any) => state.messages)
 
   return (
-    <div className='flex flex-col w-full justify-between p-10 gap-5'>
-      <div className='self-end'>
-        {messages?.sent_messages &&
-          messages.sent_messages.map((message: Messages) => {
-            return (
-              <Message
-                key={message.id}
-                sent_message={message}
-                users={users.users}
-                type='sent'
-              />
-            )
-          })}
-      </div>
-      <div>
-        {messages?.received_messages &&
-          messages.received_messages.map((message: Messages) => {
-            return (
-              <Message
-                key={message.id}
-                received_message={message}
-                users={users.users}
-                type='received'
-              />
-            )
-          })}
-      </div>
+    <div className='bg-gray-100'>
+      <Header />
+      <main className='w-full h-screen flex flex-col items-start gap-8 bg-gray-100 py-5 justify-start px-[200px] max-2xl:px-[150px] max-lg:px-[80px] max-md:px-[35px] max-sm:px-[20px]'>
+        <h2 className='mx-auto text-2xl font-semibold w-full'>შეტყობინებები</h2>
+        <div className='flex flex-col w-full  mx-auto bg-white shadow-xl rounded-2xl justify-between py-16 px-10 gap-5'>
+          <div className='self-end'>
+            {messages?.sent_messages &&
+              messages.sent_messages.map((message: Messages) => {
+                return (
+                  <Message
+                    key={message.id}
+                    sent_message={message}
+                    users={users.users}
+                    type='sent'
+                  />
+                )
+              })}
+          </div>
+          <div>
+            {messages?.received_messages &&
+              messages.received_messages.map((message: Messages) => {
+                return (
+                  <Message
+                    key={message.id}
+                    received_message={message}
+                    users={users.users}
+                    type='received'
+                  />
+                )
+              })}
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { FiShoppingCart, FiPlusCircle, FiUser } from 'react-icons/fi'
 import { FaRegHeart } from 'react-icons/fa'
 import { MdOutlineMailOutline } from 'react-icons/md'
@@ -6,6 +8,8 @@ import icon from '../assets/logo.png'
 import Image from 'next/image'
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { IoIosSearch } from 'react-icons/io'
+import { usePathname } from 'next/navigation'
+import { FaCirclePlus } from 'react-icons/fa6'
 
 const links = [
   { icon: MdOutlineMailOutline, href: '/inbox' },
@@ -14,6 +18,7 @@ const links = [
 ]
 
 const Header = () => {
+  const pathname = usePathname()
   return (
     <header className='flex flex-col items-start gap-9 px-[200px] max-2xl:px-[150px] max-lg:px-[80px] max-md:px-[35px] max-sm:px-[20px]'>
       <div className='w-full flex items-center justify-between relative'>
@@ -31,7 +36,11 @@ const Header = () => {
             href='/sell-item'
             className='bg-[#FFF4CC] flex hover:bg-[#FFEFB2] cursor-pointer transition-all ease-linear items-center h-[42px] w-[130px] justify-center rounded-xl gap-2'
           >
-            <FiPlusCircle className='text-mainYellow text-[20px]' />
+            {pathname === '/' ? (
+              <FiPlusCircle className='text-mainYellow text-[20px]' />
+            ) : (
+              <FaCirclePlus className='text-mainYellow text-[20px]' />
+            )}
             <span className='text-sm'>დამატება</span>
           </Link>
           <div className='flex items-center gap-3'>
@@ -65,19 +74,21 @@ const Header = () => {
           </SignedIn>
         </nav>
       </div>
-      <div className='flex flex-col items-start gap-6 w-full'>
-        <h2 className='text-[27px] font-medium'>ყველაფერი, რასაც ეძებ</h2>
-        <div className='flex items-center gap-4 w-full'>
-          <input
-            className='border-[3px] flex-grow px-5 py-5 placeholder:text-lg outline-none border-mainYellow rounded-xl'
-            placeholder='მაგ: Iphone 14'
-          />
-          <button className='flex items-center bg-[#FEC900] hover:bg-[#ffde68] cursor-pointer transition-all ease-linear py-5 rounded-xl px-12 gap-2 text-[18px]'>
-            <IoIosSearch />
-            <span>ძებნა</span>
-          </button>
+      {pathname === '/' && (
+        <div className='flex flex-col items-start gap-6 w-full'>
+          <h2 className='text-[27px] font-medium'>ყველაფერი, რასაც ეძებ</h2>
+          <div className='flex items-center gap-4 w-full'>
+            <input
+              className='border-[3px] flex-grow px-5 py-5 placeholder:text-lg outline-none border-mainYellow rounded-xl'
+              placeholder='მაგ: Iphone 14'
+            />
+            <button className='flex items-center bg-[#FEC900] hover:bg-[#ffde68] cursor-pointer transition-all ease-linear py-5 rounded-xl px-12 gap-2 text-[18px]'>
+              <IoIosSearch />
+              <span>ძებნა</span>
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </header>
   )
 }
