db5f369f61a56bc79f8bae3aeb29acf2f577dbc9
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-06-15 11:42:33 +0400

    commiting cause i have to go today and just in case i cant loose my streak

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/Header'
+
+const UserCartPage = () => {
+  return (
+    <>
+      <Header />
+      <div>Cart</div>
+    </>
+  )
+}
+
+export default UserCartPage
