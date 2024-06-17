8cfbfd24c53d2db1fbfc9a71d5f6c37f4624db19
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-06-18 00:51:14 +0400

    Cart product component design is done

--- src/app/cart/_components/Product.tsx
+++ src/app/cart/_components/Product.tsx
@@ -1,4 +1,8 @@
 import { Product } from '@prisma/client'
+import Image from 'next/image'
+import { HiOutlinePlusSm } from 'react-icons/hi'
+import { FiMinus, FiDollarSign } from 'react-icons/fi'
+import { FaLariSign } from 'react-icons/fa6'
 
 const CartProduct = ({
   product,
@@ -7,8 +11,37 @@ const CartProduct = ({
   product: Product
   count: number
 }) => {
-  const { name, price, images, youtubeURL, description, bill } = product
-  return <div>Product</div>
+  const { name, price, images, description, bill } = product
+  return (
+    <div className='flex flex-col gap-5 items-start'>
+      <div className='flex flex-col items-start gap-2 w-[300px]'>
+        <Image
+          src={images[0]}
+          alt='product image'
+          className='object-cover'
+          width={150}
+          height={120}
+        />
+
+        <h3 className='text-[17px] font-medium'>{name}</h3>
+      </div>
+      <div className='flex items-center gap-3'>
+        <button className='p-0.5 rounded bg-mainYellow text-white group'>
+          <HiOutlinePlusSm className='group-active:scale-90' />
+        </button>
+        <span>{count}</span>
+        <button className='p-0.5 rounded bg-mainYellow text-white group'>
+          <FiMinus className='group-active:scale-[85%]' />
+        </button>
+      </div>
+      <div className='flex flex-col items-start gap-5 max-w-[85%]'>
+        <div className='flex items-center gap-1 font-medium'>
+          {bill === 'ლარი' ? <FaLariSign /> : <FiDollarSign />}
+          <span>{price * count}</span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default CartProduct
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,15 +1,30 @@
 'use client'
 import Header from '@/components/Header'
 import { useSelector } from 'react-redux'
+import CartProduct from './_components/Product'
+import { Product } from '@prisma/client'
 
 const UserCartPage = () => {
-  const { cartItems } = useSelector((state: any) => state.cart)
+  const { cartItems }: { cartItems: Product[] } = useSelector(
+    (state: any) => state.cart
+  )
   return (
     <>
       <Header />
       <div className='px-[200px] max-2xl:px-[150px] max-lg:px-[80px] max-md:px-[35px] max-sm:px-[20px] flex flex-col items-start gap-5 py-5'>
         <h2 className='text-2xl font-semibold'>ჩემი კალათა</h2>
-        <div className='flex flex-col items-start w-full gap-3'></div>
+        <div className='flex flex-col items-start flex-wrap w-full gap-4'>
+          {cartItems &&
+            cartItems.map((product: any, idx: number) => {
+              return (
+                <CartProduct
+                  key={idx}
+                  product={product}
+                  count={product.count}
+                />
+              )
+            })}
+        </div>
       </div>
     </>
   )
