6a4d6bfdcbebddd74da279cb54f3e107aaa8ee68
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-06-20 17:30:59 +0400

    getting the user from the endpoint and storing him in our state

--- src/lib/slice/userSlice.ts
+++ src/lib/slice/userSlice.ts
@@ -1,20 +1,47 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { Product, User } from '@prisma/client'
+import axios from 'axios'
+import { ActionReducerMapBuilder } from '@reduxjs/toolkit'
 
 type initialType = {
   user: User | null
   user_products: Product[]
+  userLoading: boolean
 }
+
+export const fetchUser = createAsyncThunk('/user/fetch', async () => {
+  try {
+    const { data } = await axios.get('/api/user')
+    return data.user
+  } catch (err) {
+    return console.log(err)
+  }
+})
 
 const initialState: initialType = {
   user: null,
   user_products: [],
+  userLoading: false,
 }
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {},
+  extraReducers: (builder: ActionReducerMapBuilder<any>) => {
+    builder.addCase(fetchUser.pending, (state) => {
+      state.userLoading = true
+    })
+    builder.addCase(fetchUser.rejected, (state) => {
+      state.userLoading = false
+      state.user = null
+    })
+    builder.addCase(fetchUser.fulfilled, (state, { payload }) => {
+      state.user = payload
+      state.userLoading = false
+      console.log(state.user)
+    })
+  },
 })
 
 export default userSlice.reducer
--- src/provider/ProductsProvider.tsx
+++ src/provider/ProductsProvider.tsx
@@ -4,15 +4,17 @@ import { useEffect } from 'react'
 import { fetchProducts } from '@/lib/slice/productSlice'
 import { fetchMessages, fetchUsers } from '@/lib/slice/messagesSlice'
 import { renderCart, renderWishlist } from '@/lib/slice/cartSlice'
+import { fetchUser } from '@/lib/slice/userSlice'
 
 const ProductsProvider = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useDispatch()
   useEffect(() => {
+    dispatch(renderCart())
+    dispatch(renderWishlist())
     dispatch(fetchProducts() as any)
     dispatch(fetchMessages() as any)
     dispatch(fetchUsers() as any)
-    dispatch(renderCart())
-    dispatch(renderWishlist())
+    dispatch(fetchUser() as any)
   }, [])
   return <>{children}</>
 }
