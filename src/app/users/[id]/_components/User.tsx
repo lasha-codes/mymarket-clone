83de869c6ace36722b883bd4b7d273fce95aef13
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-06-21 14:01:14 +0400

    UserProfile component is done

--- src/app/users/[id]/_components/User.tsx
+++ src/app/users/[id]/_components/User.tsx
@@ -1,8 +1,40 @@
 'use client'
+import { User } from '@prisma/client'
 import { useSelector } from 'react-redux'
+import Image from 'next/image'
 
-const User = ({ userId }: { userId: string }) => {
-  return <div></div>
+const UserProfile = () => {
+  const { dbUsers, user }: { dbUsers: User[] | null; user: User | null } =
+    useSelector((state: any) => state.user)
+  const userInfo: any =
+    dbUsers &&
+    dbUsers.find((account: User) => {
+      return account.id === user?.id
+    })
+  if (!user) {
+    return (
+      <div className=''>
+        <h3>No account found</h3>
+      </div>
+    )
+  }
+  return (
+    <div className='w-full justify-between px-6'>
+      <div className='flex items-center gap-8'>
+        <div className='rounded-full h-[50px] w-[50px] relative overflow-hidden'>
+          <Image
+            fill
+            src={
+              'https://img.clerk.com/eyJ0eXBlIjoiZGVmYXVsdCIsImlpZCI6Imluc18yaFNsSkE2WktEVVVMOEpHUHBHUHo4UWlnT0oiLCJyaWQiOiJ1c2VyXzJoVzFDeXZhaWZ2ZmIwMUhmUU9Ud0dkWFdXNiJ9'
+            }
+            alt=''
+            className='object-cover'
+          />
+        </div>
+        <h3 className='text-lg font-medium'>{userInfo?.username}</h3>
+      </div>
+    </div>
+  )
 }
 
-export default User
+export default UserProfile
--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,5 +1,15 @@
+import Header from '@/components/Header'
+import UserProfile from './_components/User'
+
 const UserProfilePage = () => {
-  return <div>UserProfilePage</div>
+  return (
+    <>
+      <Header />
+      <main className='py-5 px-5'>
+        <UserProfile />
+      </main>
+    </>
+  )
 }
 
 export default UserProfilePage
